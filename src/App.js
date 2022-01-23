2eff18e3e2a3348c0578d0758892f0a31dda0ced
HyeranPark99 <bhyan14@gmail.com>
2022-01-22 21:53:57 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import {NavBar, Home} from './components';
+import {InputCampuses} from './components/campuses';
+import api from '../src/api/campuses';
+import { useEffect , useState} from 'react';
+import ListCampuses from './components/campuses/ListCampuses';
+
+
 
 function App() {
+
+  const [campuses, setCampuses] = useState([]);
+
+  console.log(campuses)
+
+  const fetchCampuses = async () => {
+    const res = await api.get("/");
+    return res.data;
+  }
+
+  const addCampusHandler = async (campus) => {
+    console.log(campus);
+    const req = {
+      //id: i
+      ...campus,
+    };
+
+    const res = await api.post("/", req);
+    console.log(res);
+    setCampuses([...campuses,res.data]);
+  };
+
+
+
+
+
+  useEffect(() => {
+    const getAllCampuses = async () => {
+      const allCampuses = await fetchCampuses();
+      if(allCampuses) setCampuses(allCampuses);
+    };
+
+    getAllCampuses();
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar fixed="top" />
+      <header className="App-header"></header>
+      <Routes>
+        <Route exact path="/" element={<Home /> } />   
+        <Route exact path="/campuses" element={<ListCampuses campuses={campuses} />} />  
+        <Route exact path="/add" element={<InputCampuses campuses={campuses}
+         setCampuses={setCampuses}
+         addCampusHandler={addCampusHandler} 
+         api={api}
+         />} />             
+      </Routes>
     </div>
   );
 }
