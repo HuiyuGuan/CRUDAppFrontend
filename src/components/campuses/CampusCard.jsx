2bae06e4df23e7473364f02d0b06e50064aaaa2c
HyeranPark99 <bhyan14@gmail.com>
2022-01-23 21:48:36 -0500

    delete btn

--- src/components/campuses/CampusCard.jsx
+++ src/components/campuses/CampusCard.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 
 
 const CampusCard = (props) => {
+  const [isSubmitted, setIsSubmitted] = useState(false)
   const [campusesInfo, setCampusInfo] = useState([])
   const { id, name, imageUrl, address, description } = props.campus;
 
@@ -19,21 +21,47 @@ const CampusCard = (props) => {
       fectchData()
       
 },[]);
+
+
+if(isSubmitted){
+  setIsSubmitted(false)
+  console.log(1)
+}
+
+
+const deleteHandler =() => {
+  axios
+    .delete(`http://localhost:5000/campuses/${id}`)
+    .then((response) => {
+      console.log(response.status);
+      setIsSubmitted(true)
+    
+    })
+    .catch((e) => console.log('something wrong'))
+   
+}
+
+console.log(id)
+
+if(isSubmitted){    
+  console.log(isSubmitted)
+  return  <Navigate to="/campuses" />
+}
+
+
   return (
     <div className="card-item">
 
       <div className="card" style={{ margin: 1 + 'em' }} >
-<<<<<<< HEAD
-        <Link to={`/campuses/${props.campus.id}`}>  
-=======
       <Link to={`/campuses/${props.campus.id}`}>  
->>>>>>> 072c6d5e27fda7e3845344da8d26095236e9d23b
           <img className="card-img-top" src={imageUrl} alt="Card cap"/>
         </Link>
         <div className="card-body">
-          <h5>{id} {name}</h5>
-          <h6>{address}</h6>
+             <h5>{id} </h5>
+            <h5>{name}</h5>
+            <h6>{address}</h6>
           <p className="card-text">{description}</p>
+          <button  class="btn btn-danger" onClick={deleteHandler}>Delete</button>
         </div>
       </div>
     </div>
