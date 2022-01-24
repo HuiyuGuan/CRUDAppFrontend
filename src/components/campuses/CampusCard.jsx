9f26224188f9f465899e2af1f49be43e6bd3cd70
HyeranPark99 <bhyan14@gmail.com>
2022-01-23 21:35:21 -0500

    merged

--- src/components/campuses/CampusCard.jsx
+++ src/components/campuses/CampusCard.jsx
@@ -1,59 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { useState} from "react";
-import { Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 
 const CampusCard = (props) => {
-  const [isSubmitted, setIsSubmitted] = useState(false)
-  const {id, name, imageUrl, address, description } = props.campus || [];
+  const [campusesInfo, setCampusInfo] = useState([])
+  const { id, name, imageUrl, address, description } = props.campus;
 
-  if(isSubmitted){
-    setIsSubmitted(false)
-    console.log(1)
-  }
- 
-  
-  const deleteHandler =() => {
-    axios
-      .delete(`http://localhost:5000/campuses/${id}`)
-      .then((response) => {
-        console.log(response.status);
-        setIsSubmitted(true)
+  useEffect(() => {
+      const getId = window.location.href.split("/");
+      const id  = getId[getId.length -1]
+
+      async function fectchData(){
+      const campusInfo = await axios.get(`http://localhost:5000/campuses/${id}`)
+      setCampusInfo(campusInfo.data)
+      }
+      fectchData()
       
-      })
-      .catch((e) => console.log('something wrong'))
-     
-  }
-
-  console.log(id)
-
-  if(isSubmitted){    
-    console.log(isSubmitted)
-    return  <Navigate to="/campuses" />
-  }
-
-
-
+},[]);
   return (
     <div className="card-item">
-      <div className="card" style={{margin:1 + 'em'}} >
-          <Link to={'/campuses/'+id}>  
-            <img className="card-img-top" src={imageUrl} alt="Card cap"
-             />
-          </Link>
-            <div className="card-body">
-            <h5>{id} </h5>
-            <h5>{name}</h5>
-            <h6>{address}</h6>
-            <p className="card-text">{description}</p>
-            <button  class="btn btn-danger" onClick={deleteHandler}>Delete</button>
+
+      <div className="card" style={{ margin: 1 + 'em' }} >
+        <Link to={`/campuses/${props.campus.id}`}>  
+          <img className="card-img-top" src={imageUrl} alt="Card cap"/>
+        </Link>
+        <div className="card-body">
+          <h5>{id} {name}</h5>
+          <h6>{address}</h6>
+          <p className="card-text">{description}</p>
         </div>
       </div>
     </div>
 
-    
-    
+
+
   );
 };
 
